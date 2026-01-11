512842aa3c5d440c3382b36c6c4c060e2d843fa7
Ashutosh <ashutoshdhondkar@gmail.com>
2026-01-11 17:08:46 +0530

    code changes done

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ ROOT_PATH = Path(__file__).parent
 # since authentication cookies are shared across threads
 MAX_TWITTER_THREADS = 2
 # number of times to scroll the twitter page to load more tweets
-MAX_TWITTER_PAGE_SCROLLS = 100
+MAX_TWITTER_PAGE_SCROLLS = 50
 
 
 SCRAPPED_DATA_FOLDER_NAME = "data/scrapped_data"
